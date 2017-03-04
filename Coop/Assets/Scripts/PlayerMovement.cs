aabf529bd99711cea72f5cea1aa366846b51ac89
Francisco Rojo <mojoz0101@gmail.com>
2017-03-04 13:34:23 -0500

    Initial controlling with ps4 controller

---
+++ Coop/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float max_speed;
+	public float speed = 1;
+
+	Vector2 acceleration;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// TEST
+		if (Input.GetKey(KeyCode.JoystickButton0)) {
+			print ("Hello world!");
+		}
+
+		//float left_stick_x = Input.GetAxis ("Horizontal");
+		//flaot left_stick_y = Input.GetAxis ("Vertical");
+
+		Vector3 left_stick_input = 
+			new Vector3 (Input.GetAxis ("Horizontal"),
+			             Input.GetAxis ("Vertical"), 0);
+
+		// Do movement
+		transform.position += left_stick_input * speed;
+	}
+}
