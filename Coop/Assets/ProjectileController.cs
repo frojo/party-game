619f9461d99203b6ada6cc22d99091b33dffad56
Francisco Rojo <mojoz0101@gmail.com>
2017-03-04 14:31:45 -0500

    You can shoot projectiles pew pew

---
+++ Coop/Assets/ProjectileController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour {
+
+	public Vector3 direction { get; set; }
+
+	public float speed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += direction * speed * Time.deltaTime;
+	}
+		
+}
--- Coop/Assets/Scripts/PlayerMovement.cs
+++ Coop/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour {
 	public float max_speed;
 	public float speed = 1;
 
+	public GameObject projectile;
+
 	Vector2 acceleration;
 
 	// Use this for initialization
@@ -16,18 +18,26 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		// TEST
-		if (Input.GetKey(KeyCode.JoystickButton0)) {
-			print ("Hello world!");
-		}
-
-		//float left_stick_x = Input.GetAxis ("Horizontal");
-		//flaot left_stick_y = Input.GetAxis ("Vertical");
-
 		Vector3 left_stick_input = 
 			new Vector3 (Input.GetAxis ("Horizontal"),
 			             Input.GetAxis ("Vertical"), 0);
 
+
+		if (Input.GetKeyDown(KeyCode.JoystickButton0)) {
+			GameObject projectile_obj = Instantiate (projectile);
+			// TODO(frojo): Instantiate this a little bit in front of player
+			projectile_obj.transform.position = transform.position;
+
+			if (left_stick_input == Vector3.zero) {
+				Vector3 projectile_direction = new Vector3 (0, -1, 0);
+			} else {
+				Vector3 projectile_direction = left_stick_input;;
+			}
+
+			projectile_obj.GetComponent<ProjectileController> ().direction =
+				left_stick_input;
+		}
+			
 		// Do movement
 		transform.position += left_stick_input * speed;
 	}
